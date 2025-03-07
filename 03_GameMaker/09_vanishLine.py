02de15b4754fad2087d410e5bbce896f15bb7e4d
chyijiunn <chyijiunn@gmail.com>
2025-03-07 15:24:23 +0800

    add

---
+++ 03_GameMaker/09_vanishLine.py
@@ -0,0 +1,32 @@
+#shoot
+from machine import Pin, PWM
+from utime import sleep
+import touch
+LCD = touch.LCD_1inch28()
+color = LCD.color
+LCD.set_bl_pwm(35535)
+Touch=touch.Touch_CST816T(mode=1,LCD=LCD)
+Touch.tim.init(period=1, callback=Touch.Timer_callback)
+Touch.Flag = 0
+LCD.fill(color(0,0,0))
+def shoot(x,y):
+    shootsize = 30
+    for j in range(y/shootsize):
+        for i in range(shootsize):
+            LCD.pixel(x,y-(shootsize*j)-i,color(150,150,20))
+            LCD.show()
+        for i in range(shootsize):
+            LCD.pixel(x,y-(shootsize*j)-i,color(0,0,0))
+            LCD.show()
+        
+while 1:
+    
+    if Touch.Flag == 1:
+        x , y = Touch.X_point , Touch.Y_point
+        shoot(x,y)
+        #LCD.write_text('Touch',65 , 120,2 , color(200,200,200))
+        Touch.Flag = 0
+    LCD.show()
+    sleep(0.02)
+ 
+
--- 03_GameMaker/11_multuLine.py
+++ 03_GameMaker/11_multuLine.py
@@ -1,7 +1,7 @@
-#shoot
+#新增 list ，超過某數量刪除前面紀錄點
 from machine import Pin, PWM
 from utime import sleep
-import touch
+import touch , _thread
 LCD = touch.LCD_1inch28()
 color = LCD.color
 LCD.set_bl_pwm(35535)
@@ -9,24 +9,24 @@ Touch=touch.Touch_CST816T(mode=1,LCD=LCD)
 Touch.tim.init(period=1, callback=Touch.Timer_callback)
 Touch.Flag = 0
 LCD.fill(color(0,0,0))
+position = []
 def shoot(x,y):
-    shootsize = 100
+    shootsize = 20
     for j in range(y/shootsize):
         for i in range(shootsize):
-            LCD.pixel(x,y-(shootsize*j)-i,color(150,150,20))
+            LCD.vline(x,y-(shootsize*j),shootsize,color(150,150,20))
             LCD.show()
         for i in range(shootsize):
-            LCD.pixel(x,y-(shootsize*j)-i,color(0,0,0))
-            LCD.show()
+            LCD.vline(x,y-(shootsize*j),shootsize,color(0,0,0))       
         
 while 1:
-    
     if Touch.Flag == 1:
         x , y = Touch.X_point , Touch.Y_point
-        shoot(x,y)
-        #LCD.write_text('Touch',65 , 120,2 , color(200,200,200))
+        position.append([x,y])
+        print(position)
         Touch.Flag = 0
-    LCD.show()
-    sleep(0.02)
+        if len(position)>=3:del position[-3:]
+        sleep(0.05)
  
+
 
