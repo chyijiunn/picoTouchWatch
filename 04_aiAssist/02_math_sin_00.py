8ed3bec043d5a18d19180b2401a2bf5fcd62cbe8
chyijiunn <chyijiunn@users.noreply.github.com>
2026-01-18 08:47:20 +0800

    rename

--- 04_aiAssist/02_math_sin_00.py
+++
@@ -1,6 +0,0 @@
-import math
-radian = math.radians(30)#角度轉弧度
-value_1 = math.sin(radian)#sin需讀取弧度
-print(value_1)
-
-y = 0.5 # y = sin(x) , what is x ?
--- 04_aiAssist/02_math_sin_01_asin.py
+++
@@ -1,8 +0,0 @@
-import math
-radian = math.radians(30)
-value_1 = math.sin(radian)
-print(value_1)
-
-y = 0.5 # y = sin(x) , what is x ?
-value_2 = math.asin(y)#asin 可反求弧度
-print(value_2)
