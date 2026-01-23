8d8b4f61fb1780ccf59c33770a4726b5f2b34324
chyijiunn <chyijiunn@gmail.com>
2026-01-23 12:00:01 +0800

    Create 045_logfix.py

---
+++ 04_aiAssist/045_logfix.py
@@ -0,0 +1,15 @@
+import time, math
+from hw import LCD, IMU
+c = LCD.color(255,255,0)
+data = open('record','a') 
+walkNum = 0 # 計步器 = 0
+while 1:
+    LCD.fill(0)
+    yg0 = IMU.Read_XYZ()[1]-1
+    time.sleep(0.05)
+    yg1 = IMU.Read_XYZ()[1]-1
+    if yg0 * yg1 < 0:
+        walkNum = walkNum + 1
+        LCD.write_text(str(walkNum),120,120,3,c)
+        LCD.show()
+    
