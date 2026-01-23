600cbd2e6a1bf1f42cf031f9ad55122209f9c065
chyijiunn <chyijiunn@gmail.com>
2026-01-23 16:02:45 +0800

    Update hw.py

--- lib/hw.py
+++ lib/hw.py
@@ -1,10 +1,11 @@
+# hw.py
 import touch
-
+# ===== LCD =====
 LCD = touch.LCD_1inch28()
 LCD.set_bl_pwm(15535)
-
+# ===== IMU =====
 IMU = touch.QMI8658()
-
-TP = touch.Touch_CST816T(mode=1,i2c_num=1,i2c_sda=touch.I2C_SDA,i2c_scl=touch.I2C_SDL,int_pin=touch.I2C_INT,rst_pin=touch.I2C_RST)
+# ===== Touch Panel =====
+TP = touch.Touch_CST816T(mode=1, LCD=LCD)
 TP.Mode = 1
 TP.Set_Mode(1)
